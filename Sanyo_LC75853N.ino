b65fcea7e6b41ac04e9f6da9e42dee7501d610ef
Regorlas <roger.random@outlook.com>
2022-05-13 16:01:00 -0700

    Blink test

--- Sanyo_LC75853N.ino
+++ Sanyo_LC75853N.ino
@@ -1,9 +1,16 @@
-void setup() {
-  // put your setup code here, to run once:
+/* Trying to control a Sanyo LC75853N LCD driver with an Arduino Nano */
 
+void setup() {
+  pinMode(13, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(13, HIGH);
+  delay(10);
+  digitalWrite(13, LOW);
+  delay(100);
+  digitalWrite(13, HIGH);
+  delay(10);
+  digitalWrite(13, LOW);
+  delay(1000);
 }
